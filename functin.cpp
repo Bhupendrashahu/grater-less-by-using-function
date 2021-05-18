8f19b0f917e441cfa098e02f87824fd3a0ff9ab8
Bhupendrashahu <76242175+Bhupendrashahu@users.noreply.github.com>
2021-05-18 18:47:29 +0530

    Add files via upload

---
+++ functin.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void cube()
+{
+	int a,b;
+	cout <<"enter the two \n";
+	cin>>a>>b;
+	if(a>b)
+	{
+		cout<<"grater is"<<a;
+	}
+	else
+	{
+		cout<<"greater is "<<b;
+	}
+	
+}
+int main()
+{
+ cube();
+}
